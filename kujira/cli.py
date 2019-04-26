48de87727b8aefef8947a2088d58598d18a765d1
Collin Meyers <cfmeyers@gmail.com>
2019-04-26 19:19:09 -0400

    Add config reader

--- kujira/cli.py
+++ kujira/cli.py
@@ -3,16 +3,20 @@
 """Console script for kujira."""
 import sys
 import click
+from kujira.kujira import get_open_issues, get_conn
 
 
 @click.command()
 def main(args=None):
     """Console script for kujira."""
-    click.echo("Replace this message by putting your code into "
-               "kujira.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    conn = get_conn()
+    issues = get_open_issues(conn)
+    for issue in issues:
+        click.echo(issue.fields.summary)
+    # click.echo("Replace this message by putting your code into " "kujira.cli.main")
+    # click.echo("See click documentation at http://click.pocoo.org/")
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    main()
--- kujira/kujira.py
+++ kujira/kujira.py
@@ -1,3 +1,33 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+from collections import namedtuple
+import configparser
+import os
+
+from jira import JIRA
+
+Config = namedtuple('Config', 'user api_key server_url')
+
+
+def read_config(config_path='~/.jira_config.ini'):
+    cfg = configparser.ConfigParser()
+    cfg.read(os.path.expanduser(config_path))
+    return Config(
+        user=cfg['Jira']['user'],
+        api_key=cfg['Jira']['api_key'],
+        server_url=cfg['Jira']['server_url'],
+    )
+
+
+def get_conn():
+    config = read_config()
+    options = {'server': config.server_url}
+
+    return JIRA(options, basic_auth=(config.user, config.api_key))
+
+
+def get_open_issues(conn):
+    return conn.search_issues(
+        'resolution = unresolved and assignee=currentuser() ORDER BY created'
+    )
